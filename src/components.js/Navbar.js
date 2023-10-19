1550a4c4d3cdfb6cf0e6338242d974e0d3f0e0fe
sumitrajkumar <sumit.kumar2022a@vitstudent.ac.in>
2023-10-19 23:50:44 +0530

    intital commit
    initail commit

---
+++ src/components.js/Navbar.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import './Navbar.css';
+function Navbar() {
+  return (
+    <>
+    <div className='main-nav'>
+      <div className='navbar'>
+        
+            <p>Home</p>
+            <p>About</p>
+            <p>Service</p>
+            <p>Contacts</p>
+     
+      </div>
+    </div>
+        <div id='fixed'><p>Hurry up buy now</p></div>
+      <h1 id='main-heading'>The Sustainable Kit</h1>
+
+      <div id='image'>
+        <img src="prob.jpg"  alt="nature-image"  loading="lazy"/>
+      </div>
+
+
+      <div id="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptas qui distinctio rerum quos soluta optio suscipit, non quo cumque consequatur repudiandae reprehenderit, impedit, perferendis quidem ducimus repellendus dolor. Ipsa!
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta possimus asperiores excepturi impedit voluptatum suscipit magnam, eaque tempora, natus blanditiis molestias. Dignissimos, maxime debitis cum tempora reprehenderit qui minus quibusdam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolore, at dolorum cupiditate eaque iure. Fuga inventore deserunt unde? Et id consequuntur dolorem eveniet cupiditate molestias sapiente ullam iure doloremque?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptas qui distinctio rerum quos soluta optio suscipit, non quo cumque consequatur repudiandae reprehenderit, impedit, perferendis quidem ducimus repellendus dolor. Ipsa!
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta possimus asperiores excepturi impedit voluptatum suscipit magnam, eaque tempora, natus blanditiis molestias. Dignissimos, maxime debitis cum tempora reprehenderit qui minus quibusdam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolore, at dolorum cupiditate eaque iure. Fuga inventore deserunt unde? Et id consequuntur dolorem eveniet cupiditate molestias sapiente ullam iure doloremque?
+    </div>
+
+
+    <div className='cards'>
+        <div className='box'>
+        <p><b>Reusable Water bottle</b></p>
+        <img src="img3.jpg" alt="bootle-image"  loading="lazy"/>
+        </div>
+
+        <div className='box'>
+        <p><b>Bamboo utensils</b></p>
+        <img src="img2.jpg" alt="utensils-image"  loading="lazy"/>
+        </div>
+
+
+        <div className='box'>
+        <p><b>Reusable grocery bags</b></p>
+        <img src="img5.jpg" alt="bags-image"  loading="lazy" />
+        </div>
+
+
+        <div className='box'>
+        <p><b>Eco friendly notebook</b></p>
+        <img src="img4.jpg" alt="notebook-image"  loading="lazy"/>
+        </div>
+
+
+        <div className='box'>
+        <p><b>Solar-powered charger</b></p>
+        <img src="img1.jpg" alt="charger-image"  loading="lazy"/>
+        </div>
+    </div>
+
+
+
+    <div id="none">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptas qui distinctio rerum quos soluta optio suscipit, non quo cumque consequatur repudiandae reprehenderit, impedit, perferendis quidem ducimus repellendus dolor. Ipsa!
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta possimus asperiores excepturi impedit voluptatum suscipit magnam, eaque tempora, natus blanditiis molestias. Dignissimos, maxime debitis cum tempora reprehenderit qui minus quibusdam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolore, at dolorum cupiditate eaque iure. Fuga inventore deserunt unde? Et id consequuntur dolorem eveniet cupiditate molestias sapiente ullam iure doloremque?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptas qui distinctio rerum quos soluta optio suscipit, non quo cumque consequatur repudiandae reprehenderit, impedit, perferendis quidem ducimus repellendus dolor. Ipsa!
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta possimus asperiores excepturi impedit voluptatum suscipit magnam, eaque tempora, natus blanditiis molestias. Dignissimos, maxime debitis cum tempora reprehenderit qui minus quibusdam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolore, at dolorum cupiditate eaque iure. Fuga inventore deserunt unde? Et id consequuntur dolorem eveniet cupiditate molestias sapiente ullam iure doloremque?
+    orem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptas qui distinctio rerum quos soluta optio suscipit, non quo cumque consequatur repudiandae reprehenderit, impedit, perferendis quidem ducimus repellendus dolor. Ipsa!
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta possimus asperiores excepturi impedit voluptatum suscipit magnam, eaque tempora, natus blanditiis molestias. Dignissimos, maxime debitis cum tempora reprehenderit qui minus quibusdam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolore, at dolorum cupiditate eaque iure. Fuga inventore deserunt unde? Et id consequuntur dolorem eveniet cupiditate molestias sapiente ullam iure doloremque?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptas qui distinctio rerum quos soluta optio suscipit, non quo cumque consequatur repudiandae reprehenderit, impedit, perferendis quidem ducimus repellendus dolor. Ipsa!
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta possimus asperiores excepturi impedit voluptatum suscipit magnam, eaque tempora, natus blanditiis molestias. Dignissimos, maxime debitis cum tempora reprehenderit qui minus quibusdam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inv
+    orem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptas qui distinctio rerum quos soluta optio suscipit, non quo cumque consequatur repudiandae reprehenderit, impedit, perferendis quidem ducimus repellendus dolor. Ipsa!
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta possimus asperiores excepturi impedit voluptatum suscipit magnam, eaque tempora, natus blanditiis molestias. Dignissimos, maxime debitis cum tempora reprehenderit qui minus quibusdam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolore, at dolorum cupiditate eaque iure. Fuga inventore deserunt unde? Et id consequuntur dolorem eveniet cupiditate molestias sapiente ullam iure doloremque?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptas qui distinctio rerum quos soluta optio suscipit, non quo cumque consequatur repudiandae reprehenderit, impedit, perferendis quidem ducimus repellendus dolor. Ipsa!
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta possimus asperiores excepturi impedit voluptatum suscipit magnam, eaque tempora, natus blanditiis molestias. Dignissimos, maxime debitis cum tempora reprehenderit qui minus quibusdam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inv
+    orem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptas qui distinctio rerum quos soluta optio suscipit, non quo cumque consequatur repudiandae reprehenderit, impedit, perferendis quidem ducimus repellendus dolor. Ipsa!
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta possimus asperiores excepturi impedit voluptatum suscipit magnam, eaque tempora, natus blanditiis molestias. Dignissimos, maxime debitis cum tempora reprehenderit qui minus quibusdam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolore, at dolorum cupiditate eaque iure. Fuga inventore deserunt unde? Et id consequuntur dolorem eveniet cupiditate molestias sapiente ullam iure doloremque?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptas qui distinctio rerum quos soluta optio suscipit, non quo cumque consequatur repudiandae reprehenderit, impedit, perferendis quidem ducimus repellendus dolor. Ipsa!
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta possimus asperiores excepturi impedit voluptatum suscipit magnam, eaque tempora, natus blanditiis molestias. Dignissimos, maxime debitis cum tempora reprehenderit qui minus quibusdam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inv
+    orem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptas qui distinctio rerum quos soluta optio suscipit, non quo cumque consequatur repudiandae reprehenderit, impedit, perferendis quidem ducimus repellendus dolor. Ipsa!
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta possimus asperiores excepturi impedit voluptatum suscipit magnam, eaque tempora, natus blanditiis molestias. Dignissimos, maxime debitis cum tempora reprehenderit qui minus quibusdam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolore, at dolorum cupiditate eaque iure. Fuga inventore deserunt unde? Et id consequuntur dolorem eveniet cupiditate molestias sapiente ullam iure doloremque?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptas qui distinctio rerum quos soluta optio suscipit, non quo cumque consequatur repudiandae reprehenderit, impedit, perferendis quidem ducimus repellendus dolor. Ipsa!
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta possimus asperiores excepturi impedit voluptatum suscipit magnam, eaque tempora, natus blanditiis molestias. Dignissimos, maxime debitis cum tempora reprehenderit qui minus quibusdam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inv
+    </div>
+
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default Navbar
